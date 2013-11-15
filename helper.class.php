fab6ebc4daf08bd5519aaa6bd56f606d61ceca2f
lvconggang <lvconggang@meituan.com>
2013-11-16 01:19:39 +0800

    添加拍照上传小票，添加用户分组。

--- helper.class.php
+++ helper.class.php
@@ -2,15 +2,20 @@
 include "curl.baidu.map.class.php";
 
 if(isset($_GET['k'])){
-  $helper = new helper();
+  $helper = new helper($wechat);
   $helper->debug = true;
-  echo $helper->smartSwitch($_GET['k'], 'rainsun');
+  $res =  $helper->smartSwitch($_GET['k'], 'rainsun');
+  if (is_array($res))
+    print_r($res);
+  else
+    echo $res;
 }
   
 class helper{
 
   public $debug = false;
   private $redis = Null;
+  private $wechat = null;
 
   public function helper(){
     $dbname = "xlDVkryrqFtHReOjVTud";
@@ -25,22 +30,32 @@ class helper{
   }
 
   private function getStatus($fromUsername){
-    return $this->redis->hget($fromUsername, 'status');
+    return $this->redis->hget('user_'.$fromUsername, 'status');
   }
 
   private function setStatus($fromUsername, $status){
-    return $this->redis->hset($fromUsername, 'status', $status);
+    return $this->redis->hset('user_'.$fromUsername, 'status', $status);
   }
 
   public function setLocation($fromUsername, $x, $y){
-    return $this->redis->hset($fromUsername, 'location', $x.",".$y);
+    return $this->redis->hset('user_'.$fromUsername, 'location', $x.",".$y);
   }
 
   public function getLocation($fromUsername){
-    return $this->redis->hget($fromUsername, 'location');
+    return $this->redis->hget('user_'.$fromUsername, 'location');
   }
 
-  public function smartSwitch($keyWord, $fromUsername){
+  public function updatePic($fromUsername, $picUrl){
+    $group = $this->redis->hget('user_'.$fromUsername, 'group');
+    if($group){
+      $this->redis->hset('group_'.$group, 'deal', $picUrl);
+      return "成功接收：".$group."用户组。";
+    }else{
+      return "您目前还没有任何交易记录！！";
+    }
+  }
+
+  public function smartSwitch($keyWord, $fromUsername, $option=null){
     switch ($keyWord){
       case "h":
       case "help":
@@ -63,23 +78,48 @@ class helper{
         return $this->menuGo($fromUsername, true);
         break;
 
+      case "deal":
+      case "d":
+        $status = $this->getStatus($fromUsername);
+        if ( $status == 'GO' ){
+          $this->setStatus($fromUsername, 'DEAL');
+          return $this->menuDeal($fromUsername);
+        }else{
+          return $this->smartSwitch('default', $fromUsername);
+        }
+        break;
+
+      case "join":
+      case "j":
+        $result = $this->menuJoin($fromUsername, $option[1]);
+        if($result == 1)
+          return "您已经是本组成员。";
+        elseif($result == 2)
+          return "指定用户组不存在，请仔细检查。";
+        elseif($result == 3)
+          return "必须指定团队号码";
+        else
+          return $result;
+        break;
+
+
       case "list":
       case "l":
         $this->setStatus($fromUsername, 'LIST');
       	return $this->menuList($fromUsername);
       	break;
 
+      case "welcome":
+        return $this->menuDefault();
+        break;
+
       case "r":
       case "run":
-        $res =  array(
-          0 => array(
-            'Title'=>'msg title',
-            'Description'=>'summary text',
-            'PicUrl'=>'http://tuanimg6.baidu.com/data/tuan_f013aa089b9e6270d7ed37db85376acb',
-            'Url'=>'http://www.baidu.com'
-            ),
-          );
-        return $res;
+        break;
+
+      case "del":
+        $this->redis->delete('user_'.$fromUsername);
+        return "ok";
         break;
       
       default: 
@@ -98,34 +138,59 @@ class helper{
   
   private function menuGo($fromUsername, $next){
     if(!$next){
-      $name = $this->redis->hget($fromUsername, 'name');
-      if($name){
-        return $name."已经被你选中了，换一个请输入next";
+      $uid = $this->redis->hget('user_'.$fromUsername, 'uid');
+      if($uid){
+        $picurl = $this->redis->hget('uid_'.$uid, 'picurl');
+        $name = $this->redis->hget('uid_'.$uid, 'name');
+        if($picurl){
+          $addr = $this->redis->hget('uid_'.$uid, 'addr');
+          $url = $this->redis->hget('uid_'.$uid, 'url');
+          $ret = array(
+            0 => array(
+              'Title' => "【已选中】".$name,
+              'Description' => $addr."。  需要更换请输入\"next\"\n  确认去腐败请输入\"deal\"",
+              'PicUrl' => $picurl,
+              'Url' => $url,
+              ),
+            );
+          return $ret;
+        }else
+          return $name."已经被你选中了，换一个请输入\"next\"\n  确认去腐败请输入\"deal\"";
       }
     }
   	$result = $this->randomResult($fromUsername);
 
     if($this->debug)
       print_r($result);
+    $poi = new curlbaidumap();
+    $event = $poi->getTuanGouInfo( $result['uid'] );
+    
+    $this->redis->hset('user_'.$fromUsername, 'uid', $result['uid']);
+    if(!$this->redis->hExists('uid_'.$uid, 'name')){
+      $this->redis->hset('uid_'.$result['uid'], 'name', $result['name']);
+      $this->redis->hset('uid_'.$result['uid'], 'addr', $result['address']);
+      $this->redis->hset('uid_'.$result['uid'], 'url', $result['detail_info']['detail_url']);
+      if($event)
+        $this->redis->hset('uid_'.$result['uid'], 'picurl', urldecode($event[0]['groupon_image']));
+      $this->redis->setTimeout('uid_'.$uid, 60*60*48);
+    }
+    $this->redis->setTimeout('user_'.$fromUsername, 60*60*4);
+    
 
-    $this->redis->hset($fromUsername, 'uid', $result['uid']);
-    $this->redis->hset($fromUsername, 'name', $result['name']);
-    $this->redis->hset($fromUsername, 'addr', $result['address']);
-    $this->redis->hset($fromUsername, 'url', $result['detail_info']['detail_url']);
-    $this->redis->setTimeout($fromUsername, 60*60*4);
 
-
+    if( $event ){
       $news = array(
-        0 => array(
-          'Title' => $result['name'],
-          'Description' => $result['address'],
-          'PicUrl' => "http://api.map.baidu.com/staticimage?center=".$result['location']['lng'].",".$result['location']['lat']."&width=150&height=150&zoom=13",
-          'Url' => $result['detail_info']['detail_url'],
-        ),
+          0 => array(
+            'Title' => $result['name'],
+            'Description' => $result['address'],
+            'PicUrl' => urldecode( $event[0]['groupon_image'] ),
+            'Url' => $result['detail_info']['detail_url'],
+          ),
       );
-      //$news[0] = $news;
-    //return $result['name']. $result['address']."<".$result['detail_info']['detail_url'].">";
-    return $news;
+      return $news;
+    }else{
+      return $result['name']. $result['address']."<".$result['detail_info']['detail_url'].">";
+    }
   }
   
   private function menuList($fromUsername){
@@ -159,11 +224,88 @@ class helper{
     return $str;
   }
   
+  private function menuJoin($fromUsername, $group){
+    if(isset($group)){
+      $userGroup = $this->redis->hget('user_'.$fromUsername, 'group');
+      if($userGroup){
+        if($userGroup != $group){
+          $status = $this->redis->hget('user_'.$fromUsername, 'status');
+          if($status != 'JOIN'){
+            $this->setStatus($fromUsername, 'JOIN');
+            return "您已经加入了".$userGroup."组，如果确认加入新组请再次输入join";
+          }else{
+            return $this->joinGroup($fromUsername, $group);
+          }
+        }else{
+          return 1;
+        }
+      }else{
+        //尚未加入group
+        return $this->joinGroup($fromUsername, $group);
+      }
+    }else{
+      return 3;
+    }
+  }
+
+  private function menuDeal($fromUsername){
+    $group = $this->redis->hget('user_'.$fromUsername, 'group');
+    $uid = $this->redis->hget('user_'.$fromUsername, 'uid');
+    if($group){
+      $this->redis->hset('group_'.$group, 'uid', $uid);
+    }else{
+      $groupNow = $this->redis->get('groupnow');
+      if(!$groupNow){
+        $this->redis->set('groupnow', 1000);
+      }else{
+        $this->redis->set('groupnow', $groupNow+1);
+        $this->redis->hset('user_'.$fromUsername, 'group', $groupNow);
+        $this->redis->hset('group_'.$groupNow, 'uid', $uid);
+        $this->redis->hset('group_'.$groupNow, 'membernum', '1');
+        $this->redis->hset('group_'.$groupNow, '1', $fromUsername);
+        //TODO 组没有过期。。
+        $group = $groupNow;
+      }
+    }
+    return $group."订单已创建，腐败结束后不要忘了发送小票照片呦。";
+  }
+
   private function menuDefault() {
   	$res = "欢迎使用午餐吃点啥：\n输入h或者help会有帮助!\n";
     return $res;
   }
-  
+
+  public function joinGroup($fromUsername, $group){
+    if($this->redis->hExists('group_'.$group, 'uid')){
+      $this->redis->hset('user_'.$fromUsername, 'group', $group);
+      $uid = $this->redis->hget('group_'.$group, 'uid');
+      //TODO: group 成员记录
+      $this->redis->hset('user_'.$fromUsername, 'uid', $uid);
+      $deal = $this->redis->hget('group_'.$group, 'deal');
+      if($deal){
+        $news = array(
+          0 => array(
+            'Title' => "本次对账单",
+            'Description' => "本次腐败的战果",
+            'PicUrl' => $deal,
+            'Url' => $deal,
+          ),
+        );
+        $this->setStatus($fromUsername, 'DEAL');
+        return $news;
+      }else{
+        $name = $this->redis->hget('uid_'.$uid, 'name');
+        $addr = $this->redis->hget('uid_'.$uid, 'addr');
+        //$picurl = $this->redis->hget('uid_'.$uid, 'picurl');
+        //$url = $this->redis->hget('uid_'.$uid, 'url');
+        return "已经选定".$name."[".$addr."]";
+      }
+    }else{
+      return 2;
+    }
+
+  }
+
   public function randomResult($fromUsername){
   	$poi = new curlbaidumap();
     $location = $this->getLocation($fromUsername);
@@ -182,7 +324,7 @@ class helper{
       	return $res[$rand];
     }else{
     	return "Please try again!";
-	}
+	 }
   }
 }
 ?>
--- weixin.php
+++ weixin.php
@@ -14,8 +14,14 @@ $type = $wechat->getRev()->getRevType();
 switch($type){
   case Wechat::MSGTYPE_TEXT:
     include "helper.class.php";
-    $helper = new helper();
-    $reply = $helper->smartSwitch($wechat->getRevContent(), $wechat->getRevFrom());
+    $helper = new helper($wechat);
+    $revContent = $wechat->getRevContent();
+    if(strpos($revContent, ' ')){
+      $result = explode(' ', $revContent);
+      $reply = $helper->smartSwitch($result[0], $wechat->getRevFrom(), $result);
+    }else{
+      $reply = $helper->smartSwitch($revContent, $wechat->getRevFrom(), null);
+    }
     if(is_array($reply))
       $wechat->news($reply)->reply();
     else
@@ -33,7 +39,20 @@ switch($type){
       $wechat->text("定位失败，请重试")->reply();
     break;
   case Wechat::MSGTYPE_IMAGE:
-    $wechat->text("图片功能正在开发中!!")->reply();
+    include "helper.class.php";
+    $url = $wechat->getRevPic();
+    $helper = new helper();
+    $result = $helper->updatePic($wechat->getRevFrom(), $url);
+    $wechat->text($result)->reply();
+    break;
+  case Wechat::MSGTYPE_EVENT:
+    $ret = $wechat->getRevEvent();
+    if($ret['event'] == 'subscribe'){
+      include "helper.class.php";
+      $helper = new helper();
+      $reply = $helper->smartSwitch('welcome', $wechat->getRevFrom(), null);
+    }
+    $wechat->text($reply)->reply();
     break;
   default:
     $weObj->text("help info")->reply();
